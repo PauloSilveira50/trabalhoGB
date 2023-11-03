8caba56e1681c39cb08901a0c307c8f65a0cd59b
Ígor Douglas <igdouglas.silva@gmail.com>
2023-11-02 21:57:12 -0300

    Processo

--- classes/Processo.py
+++ classes/Processo.py
@@ -1,9 +1,7 @@
 class Processo:
-    def __init__(self, pid:int, tipoProcesso:str=None):
-        self.pid = int(pid)
-        self.tipoProcesso = None
-        if tipoProcesso != None:
-            self.tipoProcesso = str(tipoProcesso)
+    def __init__(self, pid: int, tipoProcesso: str = None):
+        self.pid = pid
+        self.tipoProcesso = tipoProcesso
 
     def getPid(self):
         return self.pid
