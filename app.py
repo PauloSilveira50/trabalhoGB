9a1f08bf178aaeea0d3a94a131cf6580db5142a1
Ígor Douglas <igdouglas.silva@gmail.com>
2023-11-02 21:57:01 -0300

    app

--- app.py
+++ app.py
@@ -3,10 +3,10 @@ from classes.ProcessoCalculo import *
 from classes.ProcessoEscrita import *
 from classes.ProcessoGravacao import *
 from classes.ProcessoLeitura import *
+from classes.Sistema import *
 
 if __name__ == "__main__":
     processo1 = Processo(1)
-
-    processo1.Info()
-
     processoCalculo1 = ProcessoCalculo(processo1.getPid(), 'Calculo', 1, 1, 1)
+
+    processoCalculo1.Info()
