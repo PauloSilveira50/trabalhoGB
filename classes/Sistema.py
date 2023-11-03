cec2f01de0850bbdeb781176c5a98a3292c4d7bc
Ígor Douglas <igdouglas.silva@gmail.com>
2023-11-02 21:57:30 -0300

    Sistema

---
+++ classes/Sistema.py
@@ -0,0 +1,17 @@
+class MenuPrincipal:
+    def mainMenu():
+        print('-'*50)
+        print('''Digite o número da sua opção escolhida:
+1 - Criar Processo
+2 - Executar Próximo Processo
+3 - Executar Processo Específico
+4 - Salvar a Fila de Processos
+5 - Carregar do Arquivo a Fila de Processos''')
+        
+    def opcoesOperador():
+        print('-'*50)
+        print('''Digite o número da sua opção escolhida:
+1 - Soma
+2 - Subtração
+3 - Multiplicação
+4 - Divisão''')
